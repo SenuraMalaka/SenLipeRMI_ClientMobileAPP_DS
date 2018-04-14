3de4cafb1e64693b18719d83ebeb029a9c87e108
Senura Dissanayake <delgahadeniya.dissanayake@students.plymouth.ac.uk>
2018-04-14 23:36:59 +0530

    added textView and error handlers to display the actions taken and got

--- app/src/main/java/com/example/senura/lipermitest1/MainActivity.java
+++ app/src/main/java/com/example/senura/lipermitest1/MainActivity.java
@@ -8,22 +8,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import net.sf.lipermi.handler.CallHandler;
 import net.sf.lipermi.net.Client;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity{
 
 
-    private Button btnTest;
-    private static int count=0;
+    private Button btnStartCon;
+    //private static int count=0;
     private static TestService testService;
     private static Client client;
-    private AsyncTask atask=null;
+    //private AsyncTask atask=null;
+    private TextView statusText=null;
 
 
     @Override
@@ -37,11 +41,12 @@ public class MainActivity extends AppCompatActivity{
             StrictMode.setThreadPolicy(policy);
         }
 
-        btnTest=(Button) findViewById(R.id.button2);
+        btnStartCon=(Button) findViewById(R.id.buttonStartCon);
+        statusText =(TextView) findViewById(R.id.textViewStatus);
 
 
 
-        btnTest.setOnClickListener(new View.OnClickListener() {
+        btnStartCon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -49,25 +54,42 @@ public class MainActivity extends AppCompatActivity{
 
                 try {
                     Log.e("mainACT","inside try" );
+                    setStatusText("inside try");
+
+
                     Toast.makeText(MainActivity.this, "Inside conn my nigga", Toast.LENGTH_SHORT).show();
                     CallHandler callHandler = new CallHandler();
+
                     Log.e("mainACT","after callhandler" );
+                    setStatusText("after callhandler");
+
+
                     client = new Client("192.168.1.4", 58882, callHandler);
+
                     Log.e("mainACT","after new client" );
+                    setStatusText("after newclient");
+
                     testService = (TestService) client.getGlobal(TestService.class);
+
                     Log.e("mainACT","after testservice" );
+                    setStatusText("after testservice");
+
                     String msg = "Test";//testService.getResponse("qwe");
                     //Toast.makeText(MainActivity.this, testService.getResponse("abc"), Toast.LENGTH_SHORT).show();
                     Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
 
                     Log.e("mainACT","afterToast" );
+                    setStatusText("after toast");
 
                     //count++;
                     sendRegSen();
                     Log.e("mainACT","after sendregsen" );
+                    setStatusText("after sendregsen func");
+
                     Toast.makeText(MainActivity.this, "END conn my nigga", Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    setStatusText("\nError:\n"+e.getMessage()+"\n");
                 }
 
 
@@ -87,13 +109,33 @@ public class MainActivity extends AppCompatActivity{
 
     private void sendRegSen(){
 
-        Toast.makeText(MainActivity.this, testService.getResponse("ycma boys"), Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "count if inside", Toast.LENGTH_SHORT).show();
+
+        try {
+
+            String responseText = testService.getResponse("sen android");
+
+            Toast.makeText(MainActivity.this, responseText, Toast.LENGTH_SHORT).show();
+            setStatusText("Response got : "+responseText);
+
+
+        }catch (Exception ex){
+            setStatusText("\nError:\n"+ex.getMessage()+"\n");
+        }
+
+
         try {
             client.close();
+            setStatusText("client closed");
         } catch (IOException e) {
-            e.printStackTrace();
+            setStatusText("client not closed");
+            setStatusText("\nError:\n"+e.getMessage()+"\n");
         }
+    }
+
+
+
+    private void setStatusText(String textStatus){
+        statusText.append(textStatus+"\n");
     }
 
 
@@ -106,61 +148,61 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-    class Conn extends AsyncTask<Void, Void, MainActivity> {
-
-        @Override
-        protected MainActivity doInBackground(Void... params) {
-            Looper.prepare();
-
-            Log.e("mainACT","inside async task" );
-
-            try {
-                Log.e("mainACT","inside try" );
-                Toast.makeText(MainActivity.this, "Inside conn my nigga", Toast.LENGTH_SHORT).show();
-                CallHandler callHandler = new CallHandler();
-                Log.e("mainACT","after callhandler" );
-                client = new Client("192.168.1.4", 58882, callHandler);
-                Log.e("mainACT","after new client" );
-                testService = (TestService) client.getGlobal(TestService.class);
-                Log.e("mainACT","after testservice" );
-                String msg = "Test";//testService.getResponse("qwe");
-                //Toast.makeText(MainActivity.this, testService.getResponse("abc"), Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-
-                Log.e("mainACT","afterToast" );
-
-                //count++;
-                sendRegSen();
-                Log.e("mainACT","after sendregsen" );
-                Toast.makeText(MainActivity.this, "END conn my nigga", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Looper.loop();
-            return null;
-        }
-
-
-
-
-
-        private void sendRegSen(){
-
-            Toast.makeText(MainActivity.this, testService.getResponse("ycma boys"), Toast.LENGTH_SHORT).show();
-            Toast.makeText(MainActivity.this, "count if inside", Toast.LENGTH_SHORT).show();
-            try {
-                client.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-
-
-
-
-    }
+//    class Conn extends AsyncTask<Void, Void, MainActivity> {
+//
+//        @Override
+//        protected MainActivity doInBackground(Void... params) {
+//            Looper.prepare();
+//
+//            Log.e("mainACT","inside async task" );
+//
+//            try {
+//                Log.e("mainACT","inside try" );
+//                Toast.makeText(MainActivity.this, "Inside conn my nigga", Toast.LENGTH_SHORT).show();
+//                CallHandler callHandler = new CallHandler();
+//                Log.e("mainACT","after callhandler" );
+//                client = new Client("192.168.1.4", 58882, callHandler);
+//                Log.e("mainACT","after new client" );
+//                testService = (TestService) client.getGlobal(TestService.class);
+//                Log.e("mainACT","after testservice" );
+//                String msg = "Test";//testService.getResponse("qwe");
+//                //Toast.makeText(MainActivity.this, testService.getResponse("abc"), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//
+//                Log.e("mainACT","afterToast" );
+//
+//                //count++;
+//                sendRegSen();
+//                Log.e("mainACT","after sendregsen" );
+//                Toast.makeText(MainActivity.this, "END conn my nigga", Toast.LENGTH_SHORT).show();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            Looper.loop();
+//            return null;
+//        }
+//
+//
+//
+//
+//
+//        private void sendRegSen(){
+//
+//            Toast.makeText(MainActivity.this, testService.getResponse("ycma boys"), Toast.LENGTH_SHORT).show();
+//            Toast.makeText(MainActivity.this, "count if inside", Toast.LENGTH_SHORT).show();
+//            try {
+//                client.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//
+//
+//
+//
+//
+//    }
 
 
 
